159bd70923272b1ded1dee87ff272bc576e74c53
Terkwood <metaterkhorn@gmail.com>
2018-06-22 18:17:18 -0400

    Read MIDI file and print data

--- README.md
+++ README.md
@@ -2,10 +2,67 @@
 
 Starter project for MIDI with rust.
 
+## Usage
+
+We recommend downloading the JS Bach Goldberg Variations from https://www.opengoldbergvariations.org/.
+
+```sh
+cargo run ~/Documents/Goldberg_Variations.mid
+```
+
 ## Mac Config
 
 * Install SimpleSynth according to https://github.com/wbsoft/frescobaldi/wiki/MIDI-playback-on-Mac-OS-X
 
+## MIDI sample data
+
+We ran [rimd](https://github.com/RustAudio/rimd) test bin against the Goldberg Variations and found a lot of Note On events.
+
+```sh
+% cargo run --bin test # rimd
+
+Reading: /Documents/Goldberg_Variations.mid
+format: multiple track
+tracks: 2
+division: 480
+
+1: Track, copyright: [none], name: [none]
+events:
+  time: 0	Meta Event: Time Signature: 3/4, 24 ticks/metronome click, 8 32nd notes/quarter note
+  time: 0	Meta Event: Key Signature, 1 sharps/flats, Major
+  time: 0	Meta Event: Set Tempo, microseconds/quarter note: 833333
+  time: 0	Control Change: [121,0]	channel: Some(0)
+  time: 0	Program Change: [1]	channel: Some(0)
+  time: 0	Control Change: [7,127]	channel: Some(0)
+  time: 0	Control Change: [10,64]	channel: Some(0)
+  time: 0	Control Change: [91,0]	channel: Some(0)
+  time: 0	Control Change: [93,0]	channel: Some(0)
+  time: 0	Meta Event: MIDI Port Prefix Assignment, port: 0
+  time: 0	Note On: [79,80]	channel: Some(0)
+  time: 479	Note On: [79,0]	channel: Some(0)
+  time: 480	Note On: [79,80]	channel: Some(0)
+  time: 959	Note On: [79,0]	channel: Some(0)
+```
+
+...
+
+```sh
+  time: 3715	Note On: [67,0]	channel: Some(0)
+  time: 3720	Note On: [69,80]	channel: Some(0)
+  time: 3778	Note On: [67,80]	channel: Some(0)
+  time: 3784	Note On: [69,0]	channel: Some(0)
+  time: 3839	Note On: [67,0]	channel: Some(0)
+  time: 3839	Note On: [69,80]	channel: Some(0)
+  time: 3899	Note On: [67,80]	channel: Some(0)
+  time: 3900	Note On: [69,0]	channel: Some(0)
+  time: 3957	Note On: [67,0]	channel: Some(0)
+  time: 3957	Note On: [69,80]	channel: Some(0)
+```
+
+We can see by reading http://www.onicos.com/staff/iz/formats/midi-event.html that most of these are "Channel 1 Note On".
+
 ## Acknowledgements
+
+Bach's Goldberg Variations are [available under Creative Commons License here](https://www.opengoldbergvariations.org/).
 
 Big thanks to [midir library](https://github.com/Boddlnagg/midir).
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,173 @@
 extern crate midir;
-
-use std::thread::sleep;
-use std::time::Duration;
-use std::io::{stdin, stdout, Write};
-use std::error::Error;
+extern crate rimd;
 
 use midir::MidiOutput;
+use rimd::{Event, SMFError, TrackEvent, SMF};
+use std::env::{args, Args};
+use std::error::Error;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
 
-fn main() {
-    match run() {
-        Ok(_) => (),
-        Err(err) => println!("Error: {}", err.description())
+mod channels;
+
+const DEFAULT_VEC_CAPACITY: usize = 133000;
+
+#[derive(Debug)]
+pub enum ChannelNote {
+    On(u8),
+    Off(u8),
+}
+
+impl ChannelNote {
+    pub fn new(channel: u8) -> Option<ChannelNote> {
+        if channel >= channels::CHANNEL_OFF_FIRST && channel <= channels::CHANNEL_OFF_LAST {
+            Some(ChannelNote::Off(channel))
+        } else if channel >= channels::CHANNEL_ON_FIRST && channel <= channels::CHANNEL_ON_LAST {
+            Some(ChannelNote::On(channel))
+        } else {
+            None
+        }
     }
 }
 
-fn run() -> Result<(), Box<Error>> {
-    let midi_out = MidiOutput::new("My Test Output")?;
-    
+#[derive(Debug)]
+pub struct MidiNote {
+    channel_note: ChannelNote,
+    time: u64,
+    vtime: u64,
+    note: u8,
+    velocity: u8,
+}
+
+fn main() {
+    let mut args: Args = args();
+    args.next();
+    let pathstr = &match args.next() {
+        Some(s) => s,
+        None => panic!("Please pass a path to an SMF to test"),
+    }[..];
+
+    let track_events = load_midi_file(pathstr);
+    println!("We have {} track events", track_events.len());
+
+    let timed_midi_messages = midi_messages_from(track_events);
+    println!("We have {} timed midi messages", timed_midi_messages.len());
+
+    for i in 0..20 {
+        println!("\t{:?}", timed_midi_messages[i])
+    }
+
+    let notes = notes_in_channel(timed_midi_messages);
+
+    println!("We have {} PlayNote instructions", notes.len());
+    for n in notes.iter() {
+        println!("\tvtime hex {:04X} ... {:?}", n.vtime, n);
+    }
+
+    match run(notes) {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err.description()),
+    }
+}
+
+fn load_midi_file(pathstr: &str) -> Vec<TrackEvent> {
+    let mut events: Vec<TrackEvent> = Vec::with_capacity(DEFAULT_VEC_CAPACITY);
+    let mut count: u16 = 0;
+    match SMF::from_file(&Path::new(&pathstr[..])) {
+        Ok(smf) => {
+            for track in smf.tracks.iter() {
+                let mut time: u64 = 0;
+                for event in track.events.iter() {
+                    if count < 200 {
+                        // TODO remove
+                        count += 1;
+                        println!(
+                            "  vtime {} event {}",
+                            event.vtime,
+                            event.fmt_with_time_offset(time)
+                        );
+                    }
+                    events.push(event.clone());
+                    time += event.vtime;
+                }
+            }
+        }
+        Err(e) => match e {
+            SMFError::InvalidSMFFile(s) => {
+                println!("{}", s);
+            }
+            SMFError::Error(e) => {
+                println!("io: {}", e);
+            }
+            SMFError::MidiError(e) => {
+                println!("Midi Error: {}", e);
+            }
+            SMFError::MetaError(_) => {
+                println!("Meta Error");
+            }
+        },
+    };
+    events
+}
+
+#[derive(Debug)]
+pub struct TimedMidiMessage {
+    pub vtime: u64,
+    pub data: Vec<u8>,
+}
+
+fn midi_messages_from(track_events: Vec<TrackEvent>) -> Vec<TimedMidiMessage> {
+    let mut midi_messages: Vec<TimedMidiMessage> = Vec::with_capacity(DEFAULT_VEC_CAPACITY);
+
+    for te in track_events {
+        match te {
+            TrackEvent { vtime, event } => match event {
+                Event::Midi(m) => midi_messages.push(TimedMidiMessage {
+                    vtime,
+                    data: m.data,
+                }),
+                Event::Meta(_m) => {}
+            },
+        }
+    }
+
+    midi_messages
+}
+
+fn notes_in_channel(midi_messages: Vec<TimedMidiMessage>) -> Vec<MidiNote> {
+    let mut time: u64 = 0;
+    let mut notes: Vec<MidiNote> = Vec::with_capacity(DEFAULT_VEC_CAPACITY);
+    for msg in midi_messages {
+        time += msg.vtime;
+        if let Some(cn) = ChannelNote::new(msg.data[0]) {
+            notes.push(MidiNote {
+                channel_note: cn,
+                time: time,
+                vtime: msg.vtime,
+                note: msg.data[1],
+                velocity: msg.data[2],
+            })
+        }
+    }
+
+    notes
+}
+
+fn run(notes: Vec<MidiNote>) -> Result<(), Box<Error>> {
+    let midi_out = MidiOutput::new("Hello MIDI Bach Magic Machine")?;
+
     // Get an output port (read from console if multiple are available)
     let out_port = match midi_out.port_count() {
         0 => return Err("no output port found".into()),
         1 => {
-            println!("Choosing the only available output port: {}", midi_out.port_name(0).unwrap());
+            println!(
+                "Choosing the only available output port: {}",
+                midi_out.port_name(0).unwrap()
+            );
             0
-        },
+        }
         _ => {
             println!("\nAvailable output ports:");
             for i in 0..midi_out.port_count() {
@@ -36,7 +180,7 @@ fn run() -> Result<(), Box<Error>> {
             input.trim().parse()?
         }
     };
-    
+
     println!("\nOpening connection");
     let mut conn_out = midi_out.connect(out_port, "midir-test")?;
     println!("Connection open. Listen!");
@@ -48,15 +192,26 @@ fn run() -> Result<(), Box<Error>> {
             const VELOCITY: u8 = 0x64;
             // We're ignoring errors in here
             let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
-            sleep(Duration::from_millis(duration * 150));
+            sleep(Duration::from_millis(duration * 100));
             let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
         };
-        
-        for i in 0..66 {
-            play_note(i, 2);
+
+        // play some notes
+        loop {
+            for i in 36..76 {
+                play_note(i, 4);
+
+                play_note(i + 7, 4);
+            }
+
+            for i in 76..36 {
+                play_note(i, 4);
+
+                play_note(i + 7, 4);
+            }
         }
     }
-    sleep(Duration::from_millis(150));
+
     println!("\nClosing connection");
     // This is optional, the connection would automatically be closed as soon as it goes out of scope
     conn_out.close();
